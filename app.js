afc8cfdcf4139b2fb69ebaf28ba73482a6eb7102
3sfoor102 <3sfoor102@gmail.com>
2026-06-25 02:29:23 +0300

    Updated randomIndex to RandomWord function

--- app.js
+++ app.js
@@ -10,8 +10,9 @@
 let wordList = ['apple', 'orange', 'banana', 'mango', 'watermelon', 'grape', 'cherry', 'Strawberry']
 let guessedWord = [] //maybe I need to define it inside a specific function to avoid saving globally
 
-// this console log is just utilizing the randomIndex function, which generate a random number, and here I am using it with an array so it is picking a random number from the array, it is like this wordList[index], where I called the function to give me a random index instead of typing a specific index
-console.log(wordList[randomIndex(wordList)])
+// for (let i = 0; let i<)
+
+//console.log(wordList[randomIndex(wordList)])
 
 
  
@@ -22,11 +23,20 @@ console.log(wordList[randomIndex(wordList)])
 /*----------------------------- Event Listeners -----------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
-// The randomWord function is simply picking one random number from the array list, it will be usefuel if I decided to use multiple types of lists, e.g. food list, cars, countries, etc. Where I only need to add the list name and the function will give me a random index for the item (random word index)
-function randomIndex(listName){
-    return Math.floor(Math.random() * listName.length)
+// The randomWord function is simply picking one random words from the array list,
+// which will be usefuel if I decided to use multiple types of lists,
+// e.g. food list, cars, countries, etc.
+// Where I only need to add the list name 
+// and the function will give me a random item from the list,
+// which is a word
+function randomWord(listName){
+    let randomIndex = Math.floor(Math.random() * listName.length)
+    return listName[randomIndex]
+// the randomIndex variables saves a radnom index, which will be used to pick a random number for the word's list
+// after generating the random number and saving it inside "randomIndex" variable we are just using it with the array to pick a random word
+// it is like we are typing wordList[0] (for example the result is "Mango"), instead of a fixed number now the random function will change this to different numbers for example (wordList[2], which will give "banana")
 }
 
-
+console.log(randomWord(wordList))
 
 console.log(wordList[3])
